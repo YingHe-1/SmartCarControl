bc73dba70fa0f39370aa789b0b523c42b43c707c
Yinghe-1 <yh9250@126.com>
2022-04-06 17:52:42 +0800

    操作杆功能尚未完成

--- app/src/main/java/com/example/smartcarcontrol/MainActivity.java
+++ app/src/main/java/com/example/smartcarcontrol/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -91,7 +92,6 @@ public class MainActivity extends AppCompatActivity {
             if (action.equals(BluetoothDevice.ACTION_FOUND)) {
                 // 获取搜索到的蓝牙设备
                 device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
                 // 搜索的设备在之前没有配对过，是一个新的设备
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
                     devices.add(device);
@@ -103,4 +103,37 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+//    private void sendCode(byte[] code)
+//    {
+//        try {
+//            // 判断客户端接口是否为空
+//            if (clientSocket == null) {
+//                // 获取到客户端接口
+//                clientSocket = selectDevice
+//                        .createRfcommSocketToServiceRecord(MY_UUID);
+//                // 向服务端发送连接
+//                clientSocket.connect();
+//                // 获取到输出流，向外写数据
+//                os = clientSocket.getOutputStream();
+//                if (commected) {
+//                    commected = false;
+//                    // 实例接收客户端传过来的数据线程
+//                    thread = new ConnectedThread(clientSocket);
+//                    // 线程开始
+//                    thread.start();
+//                }
+//            }
+//            // 判断是否拿到输出流
+//            if (os != null) {
+//                os.write(code);
+//            }
+//            Toast.makeText(MainActivity.this, "发送信息成功，请查收", Toast.LENGTH_SHORT).show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            // 如果发生异常则告诉用户发送失败
+//            Toast.makeText(MainActivity.this, "发送信息失败", Toast.LENGTH_SHORT).show();
+//        }
+//
+//    }
 }
